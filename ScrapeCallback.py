5409c54874b5791a622680d9da91d226ac5f9ec5
guimingming <17771869383@163.com>
2017-10-20 09:10:08 +0800

    DOWN MODIFY

--- ScrapeCallback.py
+++ ScrapeCallback.py
@@ -3,50 +3,35 @@
 
 import csv
 import lxml.html
-import urllib.request
-import urllib.parse
-import chardet
+from down_page import download
+
+
+# import chardet
 
 class ScrapeCallback:
     def __init__(self):
         self.writer = csv.writer(open('./test.csv', 'w'))
-        self.fields = ('id', 'link', 'page')
+        self.fields = ('link', 'title')
         self.writer.writerow(self.fields)
 
-    def tt(self, url, html):
-        # chardet.detect(rawdata)
-        tree = lxml.html.fromstring(html)
+    def __call__(self, page_html, host):
+        # chardet.detect(rawdata) 查看编码类型
+        tree = lxml.html.fromstring(page_html)
         row = []
         list = tree.cssselect('div.titlelink.box>a')
         for k, title in enumerate(list):
-            #row.append(str(k)+ ','+title.get('href')+','+title.text_content().decode('utf-8').encode('GB2312'))
+            # row.append(str(k)+ ','+title.get('href')+','+title.text_content().decode('utf-8').encode('GB2312'))
 
-            row.append(title.get('href') + '\r')
+            row.append(host + title.get('href'))
+            row.append(title.text_content())
         self.writer.writerow(row)
 
 
-def download(url, user_agent='fred_spider', proxy=None, retry=3):
-    print('下载如下链接：', url)
-    headers = {'User-agent': user_agent}
-    try:
-        request = urllib.request.Request(url, headers=headers)
-
-        opener = urllib.request.build_opener()
-        if proxy:
-            proxy_params = {urllib.parse.urlparse(url).scheme: proxy}
-            opener.add_handler(urllib.request.ProxyHandler(proxy_params))
-
-        html = opener.open(request).read()
-    except urllib.request.URLError as e:
-        print('下载遇到错误：', e.reason)
-        html = None
-        if retry > 0:
-            if hasattr(e, 'code') and 400 <= e.code <= 600:
-                return download(url, retry - 1)
-    return html
 
 if __name__ == "__main__":
     html = download('https://bbs.hupu.com/bxj')
+    print(html)
+    exit()
 
     ScrapeCallback().tt('https://bbs.hupu.com/bxj', html)
 
--- down_page.py
+++ down_page.py
@@ -39,6 +39,7 @@ def find_article_url_in_page(page_url,delay_days=2, max_depth=1, user_agent='fre
     crawl_pages_queue = [page_url]
     seen = {page_url: 0}  # 防止重复
     article_url_num = 0  # 爬到的帖子数
+    num_urls = 0
 
     # rp = get_robots(page_url)
     # rp.can_fetch(user_agent, page_url) # 检查是否允许爬虫
@@ -59,39 +60,15 @@ def find_article_url_in_page(page_url,delay_days=2, max_depth=1, user_agent='fre
 
         if depth != max_depth:
             for link in links:
-                link = normalize(seed_url, link)
+                link = normalize(page_url, link)
+                if link not in seen:
+                    seen[link] = depth + 1
+                    if same_domain(page_url, link):
+                        crawl_pages_queue.append(link)
 
-    #     # check url passes robots.txt restrictions
-    #     if rp.can_fetch(user_agent, url):
-    #         throttle.wait(url)
-    #         html = download(url, headers, proxy=proxy, num_retries=num_retries)
-    #         links = []
-    #         if scrape_callback:
-    #             links.extend(scrape_callback(url, html) or [])
-    #
-    #         if depth != max_depth:
-    #             # can still crawl further
-    #             if link_regex:
-    #                 # filter for links matching our regular expression
-    #                 links.extend(link for link in get_links(html) if re.match(link_regex, link))
-    #
-    #             for link in links:
-    #                 link = normalize(seed_url, link)
-    #                 # check whether already crawled this link
-    #                 if link not in seen:
-    #                     seen[link] = depth + 1
-    #                     # check link is within same domain
-    #                     if same_domain(seed_url, link):
-    #                         # success! add this new link to queue
-    #                         crawl_queue.append(link)
-    #
-    #         # check whether have reached downloaded maximum
-    #         num_urls += 1
-    #         if num_urls == max_urls:
-    #             break
-    #     else:
-    #         print
-    #         'Blocked by robots.txt:', url
+        num_urls += 1
+        if num_urls == 500:
+            break
 
 
 def get_robots(url):
@@ -107,6 +84,13 @@ def normalize(seed_url, link):
     link, _ = urllib.parse.urldefrag(link)  # remove hash to avoid duplicates
     return urllib.parse.urljoin(seed_url, link)
 
+
+def same_domain(url1, url2):
+    """Return True if both URL's belong to same domain
+    """
+    return urllib.parse.urlparse(url1).netloc == urllib.parse.urlparse(url2).netloc
+
+
 # 频率限制
 class Delay:
     def __init__(self, delay):
@@ -116,16 +100,12 @@ class Delay:
     def wait(self, url):
         domain = urllib.parse.urlsplit(url)
         last_visit = self.domains.get(domain)
-        if (self.delay > 0 and last_visit is not None):
+        if self.delay > 0 and last_visit is not None:
             sleep_seconds = self.delay - (datetime.now() - last_visit).seconds
             if sleep_seconds > 0:
                 time.sleep(sleep_seconds)
-
         self.domains[domain] = datetime.now()
         return domain
 
 
-
-
-
-find_article_url_in_page('https://www.zhihu.com')
+#  find_article_url_in_page('https://bbs.hupu.com/lol')
