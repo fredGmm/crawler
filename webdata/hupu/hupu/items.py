edbaab2f0ce56b8fd015472ffe9803cdcbb3744a
guimingming <17771869383@163.com>
2017-12-04 17:55:13 +0800

    scrapy css xpath

--- webdata/hupu/hupu/items.py
+++ webdata/hupu/hupu/items.py
@@ -11,7 +11,15 @@ import scrapy
 class HupuItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    # id = scrapy.Field()
     title = scrapy.Field()
-    link = scrapy.Field()
-    desc = scrapy.Field()
+    # url = scrapy.Field()
+    # category = scrapy.Field()
+    # article_content = scrapy.Field()
+    # images = scrapy.Field()
+    # comment_num = scrapy.Field()
+    # browse_num = scrapy.Field()
+    num = scrapy.Field()
+
+
 
--- webdata/hupu/hupu/spiders/hupu_spider.py
+++ webdata/hupu/hupu/spiders/hupu_spider.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
 
 import scrapy
+from hupu.items import HupuItem
 
 class HupuSpider(scrapy.Spider):
     name = "hupu"
-    allowed_domains = ["dmoz.org"]
+    allowed_domains = ["hupu.com"]
     start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+        "https://bbs.hupu.com/bxj",
     ]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2]
+       for sel in response.xpath('//div[@class="show-list"]/ul[@class="for-list"]/li'):
+           # item = HupuItem()
+           # item['title'] = sel.css('.titlelink.box a::text').extract()
+           # item['num'] = sel.xpath('span[@class=ansour box]/text()').extract()
 
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+           title = sel.xpath('div[@class="titlelink box"]/a').extract()
+           # num = sel.xpath('span[@class=ansour box]/text()').extract()
+           print(title)
+           # yield item
+
